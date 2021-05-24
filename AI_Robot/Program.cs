7aca0dcc1620086cc8e7795c21293c79df8e8658
levkrymov <levkrymov@gmail.com>
2021-05-24 15:25:29 +0300

    Добавьте файлы проекта.

---
+++ AI_Robot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] answers = { "Здравствуйте", "Хорошо", "Пока" };
+            string[] questions = { "Здравствуйте", "Как дела", "Пока" };
+
+
+            while (true)
+            {
+                string question = Console.ReadLine(); // Задаем вопрос роботу - как дела
+
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    if (questions[i] == question) // Если question(вопрос) == questions[i] => answers[i]
+                    {
+                        Console.WriteLine(answers[i]); // То ответит - answer[i]
+                    }
+                }
+
+                if ("Пока" == question)
+                {
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+    
